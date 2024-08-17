8b69b00ff0d264ad6a205d96a64e9c4712b84187
Jai Mangos <jaimangos@gmail.com>
2024-08-17 23:07:30 +1000

    basic motor test - copy paste src folder. not the best sol but works for now

---
+++ tests/basic-motor-test/src/pins.h
@@ -0,0 +1,17 @@
+/**
+ * Define pins here.
+ */
+
+#pragma once
+
+// Motors
+#define ENA 5 // PWM ~
+#define IN1 6 // can use As
+#define IN2 7
+#define IN3 8
+#define IN4 9
+#define ENB 10 // PWM ~
+
+// Bluetooth
+#define RX 4
+#define TX 3
