e0aa67f8b52e46a5ddf00d983a4e9cde3343127c
Jai Mangos <jaimangos@gmail.com>
2024-08-17 11:16:43 +1000

    Add src folder

---
+++ template.h
@@ -0,0 +1,26 @@
+#ifndef FILE_H
+#define FILE_H
+
+/* code here */
+
+#endif
+
+
+// https://stackoverflow.com/a/3247093
+// also note: https://marketplace.visualstudio.com/items?itemName=vsciot-vscode.vscode-arduino
+
+// structure:
+
+
+// main.ino
+
+// config.h
+// classes.h
+
+// pins.h
+// dimensions.h
+// helpers.h ??
+
+
+// then maybe a whole folder for tests?
+// another for config?
